3fe2a46e847d05f811b277812b5953e96137d75b
nameofname <nameofemail@gmail.com>
2017-12-16 16:05:41 -0500

    added training data from https://www.youtube.com/watch?v=wNhnAogeHJQ

---
+++ __tests__/mocks/trainingDataCollegeMajor.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// taken from : https://www.youtube.com/watch?v=wNhnAogeHJQ
+
+// Output data to test :
+    // specific conditional entropy and probability :
+    // Math: P = 0.5, E = 1
+    // CS: P = 0.25, E = 0
+    // History: 0.25 = x, E = 0
+// total conditional entropy = 0.5
+
+module.exports = [
+    {
+        major: 'Math',
+        likes: true
+    }, {
+        major: 'History',
+        likes: false
+    }, {
+        major: 'CS',
+        likes: true
+    }, {
+        major: 'Math',
+        likes: false
+    }, {
+        major: 'Math',
+        likes: false
+    }, {
+        major: 'CS',
+        likes: true
+    }, {
+        major: 'Math',
+        likes: true
+    }, {
+        major: 'History',
+        likes: false
+    }
+];
