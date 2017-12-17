8b84a4ad1890b9605fb053bcfe0e2fd044b0045a
nameofname <nameofemail@gmail.com>
2017-12-17 01:53:23 -0500

    re-organizing node

--- src/DecisionTreeNode.js
+++ src/DecisionTreeNode.js
@@ -6,6 +6,7 @@ const assert = require('assert');
 /**
  * Decision Tree Node shall have properties :
  *      - Children: Array of DCT Nodes
+ *      - classAttribute : the attribute that we are trying to classify
  *      - branchAttribute : the attribute that it branches on to get children
  *      - trainingData: Array of training data objects in the current node
  *      - count: count of trainingData objects
@@ -18,11 +19,12 @@ const assert = require('assert');
  */
 class DecisionTreeNode {
 
-    constructor({ trainingData, parentCount, attributeList }) {
+    constructor({ trainingData, parentCount, attributeList, classAttribute }) {
 
         assert(Array.isArray(trainingData), 'DecisionTreeNode: trainingData must be an array');
-        assert(Number(parentCount) === parentCount, 'DecisionTreeNode: parentCount must be an number');
+        assert(Number(parentCount) === parentCount, 'DecisionTreeNode: parentCount must be a number');
         assert(Array.isArray(attributeList), 'DecisionTreeNode: attributeList must be an array');
+        assert(typeof classAttribute === 'string', 'DecisionTreeNode: classAttribute must be a string');
 
         this.trainingData = trainingData; // array of training data objects
         this.parentCount = parentCount; // count of training data in parent node
@@ -35,29 +37,34 @@ class DecisionTreeNode {
         this.entropy = null;
     }
 
-    createDataMap (trainingData, categoryField, igField) {
-        // for each piece of training data, we want to calculate the probability that the igField will be a certain value
-        // and connect that to our category field to find the relationship between the two.
-        // to that end we start by building out a map of IG values to category values.
-        // the structure looks like this :
-        // Map {
-        //      igValue1: Map {
-        //          count : <number>,
-        //          categoryValue1: <number>,
-        //          categoryValue2: <number>,
-        //          ....
-        //      },
-        //      igValue2: Map { ... },
-        //      ...
-        // }
-        //
-        // what this tells us is the number of times each category input is associated with each ig input value we are
-        // testing for
-        // in this scenario, each igValue in the map represents a potential child node - and if we decide to branch
-        // on that child node, the counts of each will become the node in question.
-        // basically each node should be a data map with a set of training data within itself !!!! and it should have a method
-        // to sub-divide based on a given ig field and category field.
-        // the IG function should be purely mathematical, this should be a part of the node.
+    /**
+     * for each piece of training data, we want to calculate the probability that the igField will be a certain value
+     * and connect that to our category field to find the relationship between the two.
+     * to that end we start by building out a map of IG values to category values.
+     * the structure looks like this :
+     * Map {
+     *      igValue1: Map {
+     *          count : <number>,
+     *          categoryValue1: <number>,
+     *          categoryValue2: <number>,
+     *          ....
+     *      },
+     *      igValue2: Map { ... },
+     *      ...
+     * }
+     *
+     * what this tells us is the number of times each category input is associated with each ig input value we are
+     * testing for
+     * in this scenario, each igValue in the map represents a potential child node - and if we decide to branch
+     * on that child node, the counts of each will become the node in question.
+     * basically each node should be a data map with a set of training data within itself !!!! and it should have a method
+     * to sub-divide based on a given ig field and category field.
+     * @param trainingData
+     * @param categoryField
+     * @param igField
+     * @returns {{dataMap: *}}
+     */
+    createStagedChildren (trainingData, categoryField, igField) {
         const dataMap = trainingData.reduce((map, obj) => {
             const { [categoryField]: catValue, [igField]: igValue } = obj;
             if (!map.get(igValue)) {
